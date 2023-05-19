1885f79dcfe638860dcd1fde5ac83bd3521ea264
rchenand <70741244+rchenand@users.noreply.github.com>
2023-05-18 17:58:53 -0700

    Update NORFlash.h

--- NORFlash.h
+++ NORFlash.h
@@ -6,16 +6,17 @@
 #define REMS 0x90 //Read electronic manufacturer and device ID
 #define QPIID 0xAF // Read Quad ID
 
-#define RDSR1 0x05 // Read Status Register 1 (S25FL Only)
-#define RDSR2 0x07 // Read Status Register 2 (S25FL Only)
+// Register Access
+#define RDSR 0x05 // Read Status Register (MX66L, MT25)
+#define RDSR1 0x05 // Read Status Register 1 (S25FLxxxL, S25FLxxxS Only)
+#define RDSR2 0x07 // Read Status Register 2 (S25FLxxxL Only)
 #define RDSCUR 0x2B //Read Security Register (MX66L Only)
 #define RFSR 0x70 //Read Flag Status Register (MT25 Only)
 
 #define EN4B 0xB7 //Enable 4-byte address mode
 #define EX4B 0xE9 //Exit 4-byte address mode
 
-// Register Access
-#define RDSR 0x05 // Read Status Register
+
 // RDSR1 Bits
 #define WIP 0 //Write in Progress
 #define WEL 1 //Write Enable Latch
@@ -60,10 +61,14 @@
 #define P_ERR 5 //Programming Error Occurred (1 == Error)
 #define E_ERR_MT25 5
 #define P_ERR_MT25 4
+#define P_ERR_S25HST 6
+#define E_ERR_S25HST 5
 
 class NORFlash {
   private:
-    pin_size_t cs;
+    pin_size_t cs1;
+    pin_size_t cs2;
+    pin_size_t cs3;
     pin_size_t reset;
     pin_size_t wp;
     uint8_t last_known_wip;
@@ -76,8 +81,8 @@ class NORFlash {
     uint8_t device_type;
     uint8_t density_code;
     uint16_t page_size = 256;
-    uint32_t block_size = 65536;
-    uint16_t sector_size = 4096;
+    uint32_t block_size = 65536; // 64 kB (default, only one that currently isn't 64KB is S25HST)
+    uint16_t sector_size = 4096; // 4 kB
     uint8_t current_sector = 0;
     uint8_t block_verbose[65536];
     int error_bytes;
@@ -90,22 +95,36 @@ class NORFlash {
 
     void erase(){
       this->mode = "Erase";
-      digitalWrite(this->cs, LOW);
+      digitalWrite(this->cs1, LOW);
       SPI.transfer(WREN); //Write Enable (must be enabled before every command)
-      digitalWrite(this->cs, HIGH);
-      digitalWrite(this->cs, LOW); 
+      digitalWrite(this->cs1, HIGH);
+      digitalWrite(this->cs1, LOW); 
       SPI.transfer(CE);
-      digitalWrite(this->cs, HIGH);
+      digitalWrite(this->cs1, HIGH);
+    }
+
+    void block_erase(){
+      this->mode = "Block Erase";
+      digitalWrite(this->cs1,LOW);
+      SPI.transfer(WREN); //WREN must be issued to set WEL
+      digitalWrite(this->cs1,HIGH);
+      digitalWrite(this->cs1, LOW);
+      SPI.transfer(BE); //Block erase, followed by address of block to erase
+      SPI.transfer(0x00);
+      SPI.transfer(0x00);
+      SPI.transfer(0x00);
+      SPI.transfer(0x00); //Start at address zero
+      digitalWrite(this->cs1, HIGH);
     }
 
     uint8_t read_byte(uint64_t addr){
-      digitalWrite(this->cs, LOW);
+      digitalWrite(this->cs1, LOW);
       SPI.transfer(READ);
       SPI.transfer((addr >> 16) & 0xFF);
       SPI.transfer((addr >> 8) & 0xFF);
       SPI.transfer(addr & 0xFF);
       uint8_t read_data = SPI.transfer(0x00);
-      digitalWrite(this->cs, HIGH);
+      digitalWrite(this->cs1, HIGH);
       return read_data;
     }
 
@@ -115,7 +134,7 @@ class NORFlash {
       this->bytes_covered = 0;
       this->current_sector = 0;
       //Read everything out of memory, expect to see 0xAA for each byte, if not then add to error count
-      digitalWrite(this->cs, LOW);
+      digitalWrite(this->cs1, LOW);
       SPI.transfer(READ);
       SPI.transfer(0x00);
       SPI.transfer(0x00);
@@ -128,7 +147,7 @@ class NORFlash {
           this->current_sector++;
         }
         uint8_t data = SPI.transfer(0x00);
-        if (data != 0xAA){
+        if (data != 0xAA){ 
           this->error_bytes++;
         }
         this->bytes_covered++;
@@ -140,27 +159,23 @@ class NORFlash {
       /*
         Notes on Program Flash Array Section 8.5
           Page Programming allows up to a page size of 256 bytes in one operation
-
           Before the PP (Page Program) command can be accepted by the device a WREN (Write Enable) command must be issued.
           After recieing the enable the WEL (Write Enable Latch) in the status regsiter is set and operations are enabled.
-
           Page Program Structure:
-
           | * * * WREN * * * | * * * PP * * * | * * * adress byte 1 * * * | * * * adress byte 2 * * * | * * * adress byte 3 * * * | * * * data... * * * |
-
           This assumes one is using the 3-byte addressing mode, 4-byte addressing is also possible and has a different command structure. 
       */
-      digitalWrite(this->cs, LOW);  
+      digitalWrite(this->cs1, LOW);  
       SPI.transfer(WREN); //Write Enable (must be enabled before every command)
-      digitalWrite(this->cs, HIGH);
-      digitalWrite(this->cs, LOW); 
+      digitalWrite(this->cs1, HIGH);
+      digitalWrite(this->cs1, LOW); 
       SPI.transfer(PP);
       SPI.transfer((addr >> 24) & 0xFF);
       SPI.transfer((addr >> 16) & 0xFF);
       SPI.transfer((addr >> 8) & 0xFF);
       SPI.transfer(addr & 0xFF);
       SPI.transfer(save_me);
-      digitalWrite(this->cs, HIGH);
+      digitalWrite(this->cs1, HIGH);
     }
 
     void write(){
@@ -172,41 +187,41 @@ class NORFlash {
       //Bytes must be written in batches of 265 bytes (one page)
       for (uint32_t i = 0; i < this->block_size; i+=128){
         //Check if we are in a new sector
-        if(i % this->sector_size == 0){
-          this->current_sector++;
+        if(i % this->sector_size == 0){ //tracking what sector currently on, can use for debugging
+          this->current_sector++; // 16 sectors total in a block for 64 KB sector
         }
-        digitalWrite(this->cs, LOW);  
+        digitalWrite(this->cs1, LOW);  
         SPI.transfer(WREN); //Write Enable (must be enabled before every command)
-        digitalWrite(this->cs, HIGH);
+        digitalWrite(this->cs1, HIGH);
         read_status();
         this->mode = "Write";
-        digitalWrite(this->cs, LOW); 
+        digitalWrite(this->cs1, LOW); 
         SPI.transfer(PP);
         SPI.transfer((i >> 24) & 0xFF);
         SPI.transfer((i >> 16) & 0xFF);
         SPI.transfer((i >> 8) & 0xFF);
         SPI.transfer(i & 0xFF);
         for (int j = 0; j < 256; j++){
-          SPI.transfer(remember_me);
+          SPI.transfer(remember_me); // 0xAA
           this->bytes_covered++;
         }
-        digitalWrite(this->cs, HIGH);        
+        digitalWrite(this->cs1, HIGH);        
       }
     }
 
     void software_reset(){
       this->mode = "Software Reset";
-      digitalWrite(this->cs, LOW);
+      digitalWrite(this->cs1, LOW);
       SPI.transfer(RSTEN);
       delay(10);
       SPI.transfer(RST);
-      digitalWrite(this->cs, HIGH);
+      digitalWrite(this->cs1, HIGH);
       enable_four_byte_addr();
     }
 
     void hardware_reset(){
       this->mode = "Hardware Reset";
-      digitalWrite(this->cs, HIGH);
+      digitalWrite(this->cs1, HIGH);
       delay(1);
       digitalWrite(this->reset, LOW);
       delay(10);
@@ -217,12 +232,12 @@ class NORFlash {
 
     void read_id(){
       //Try to read the chip id to determine who we are
-      digitalWrite(cs, LOW);
+      digitalWrite(cs1, LOW);
       SPI.transfer(RDID); // Read Idenfification
       this->mfg_id = SPI.transfer(0x00); // The Manufacturer ID 
       this->device_type = SPI.transfer(0x00); //The device type
       this->density_code = SPI.transfer(0x00); //The device density
-      digitalWrite(cs, HIGH);
+      digitalWrite(cs1, HIGH);
       delay(10);
 
       if (this->mfg_id == 0xC2){
@@ -255,6 +270,9 @@ class NORFlash {
           case 0x60:
             this->part_number = "S25FL";
             break;
+          case 0x02:
+            this->part_number = "S25FL";
+            break;
           default:
             this->part_number = "Unknown";
         }
@@ -272,6 +290,17 @@ class NORFlash {
           default:
             this->part_number = "Unknown";
         }
+        switch(this->device_type) {
+          // is this S25HLxxxL or S25HLxxxS? 
+          case 0x60:
+            this->part_number += "L";
+            break;
+          case 0x02:
+            this->part_number += "S";
+            break;
+          default:
+            this->part_number = "Unknown";
+        }       
       }
       else if (this->mfg_id == 0x20){
         //This is a Micron part
@@ -318,6 +347,29 @@ class NORFlash {
             this->part_number = "Unknown";
         }
       }
+      else if (this->mfg_id == 0x34){
+        //This is a Infineon Cypress 1GB S25HSxxxT part
+        this->mfg = "Cypress";
+        //What is the device type?
+        this->block_size = 262144; //256 kB block, 32 4kb sectors 
+        switch(this->device_type) {
+          case 0x2B:
+            this->part_number = "S25HS01GT";
+            break;
+          default:
+            this->part_number = "Unknown";
+        }
+
+        //What is the density?
+        switch(this->density_code) {
+          case 0x1B:
+            this->density = 1024; // 1 GB, 1024 MB
+            break;
+          default:
+            this->part_number = "Unknown";
+        }
+
+      }
     }
 
     void read_status(){
@@ -326,55 +378,79 @@ class NORFlash {
           From page 73 of the datasheet:
             'The host system can determine when a write, program, erase, suspend or other embedded operation is complete
             by monitoring the Write in Progress (WIP) bit in the Status Register.'
-
           Get the WIP bit from the read status register 1. Read from status register 2 to get the state of the program error (P_ERR) and erase error (E_ERR).
           Note that the WIP bit will remain 1 when the P_ERR or E_ERR bits are 1 and the device will remain busy. This can be cleared with the Software Reset, 
           Clear Staus Register or Hardware Reset pin.
       */
       if (this->mfg_id == 0xC2){
         //This is a Macronix part
-        digitalWrite(cs, LOW);
+        digitalWrite(cs1, LOW);
         SPI.transfer(RDSR);
         uint8_t status1 = SPI.transfer(0x00);
-        digitalWrite(cs, HIGH);
+        digitalWrite(cs1, HIGH);
         this->wip = (status1 >> WIP) & 0x1;
         this->wel = (status1 >> WEL) & 0x1;
-        digitalWrite(cs, LOW);
+        digitalWrite(cs1, LOW);
         SPI.transfer(RDSCUR);
         uint8_t status2 = SPI.transfer(0x00);
-        digitalWrite(cs, HIGH);
+        digitalWrite(cs1, HIGH);
         this->p_err = (status2 >> P_ERR) & 0x1;
         this->e_err = (status2 >> E_ERR) & 0x1;
       }
-      else if (this->mfg_id == 0x01){
-        //This is an Infineon part
-        digitalWrite(cs, LOW);
+      else if (this->mfg_id == 0x01 && this->device_type == 0x02){
+        //This is an Infineon part S25FL256S
+        digitalWrite(cs1, LOW);
         SPI.transfer(RDSR1);
         uint8_t status1 = SPI.transfer(0x00); //Dummy Cycles to get info from SR1NV
-        digitalWrite(cs, HIGH);
+        digitalWrite(cs1, HIGH);
+        // WIP, WEL and Erase and Program errors all in Status Register 1
         this->wip = (status1 >> WIP) & 0x1;
         this->wel = (status1 >> WEL) & 0x1;
-        digitalWrite(cs, LOW);
+        this->e_err = (status1 >> 5) & 0x1; 
+        this->p_err = (status1 >> 6) & 0x1;
+      }
+      
+      else if (this->mfg_id == 0x01 && this->device_type == 0x60){
+        //This is an Infineon part S25FL256L
+        digitalWrite(cs1, LOW);
+        SPI.transfer(RDSR1);
+        uint8_t status1 = SPI.transfer(0x00); //Dummy Cycles to get info from SR1NV
+        digitalWrite(cs1, HIGH);
+        this->wip = (status1 >> WIP) & 0x1;
+        this->wel = (status1 >> WEL) & 0x1;
+        digitalWrite(cs1, LOW);
         SPI.transfer(RDSR2);
         uint8_t status2 = SPI.transfer(0x00);
-        digitalWrite(cs, HIGH);
+        digitalWrite(cs1, HIGH);
         this->p_err = (status2 >> P_ERR) & 0x1;
         this->e_err = (status2 >> E_ERR) & 0x1;
       }
       else if (this->mfg_id == 0x20){
         //This is a Micron part
-        digitalWrite(cs, LOW);
+        digitalWrite(cs1, LOW);
         SPI.transfer(RDSR);
         uint8_t status1 = SPI.transfer(0x00);
-        digitalWrite(cs, HIGH);
+        digitalWrite(cs1, HIGH);
         this->wip = (status1 >> WIP) & 0x1;
         this->wel = (status1 >> WEL) & 0x1;
-        digitalWrite(cs, LOW);
+        digitalWrite(cs1, LOW);
         SPI.transfer(RFSR);
         uint8_t status2 = SPI.transfer(0x00);
-        digitalWrite(cs, HIGH);
+        digitalWrite(cs1, HIGH);
         this->p_err = (status2 >> P_ERR_MT25) & 0x1;
         this->e_err = (status2 >> E_ERR_MT25) & 0x1;
+      }
+      else if (this->mfg_id == 0x34){
+        //This is a Infineon Cypress part
+        digitalWrite(cs1, LOW);
+        SPI.transfer(RDSR1);
+        uint8_t status1 = SPI.transfer(0x00);
+        digitalWrite(cs1, HIGH);
+        // RDYBSY (WIP), E_ERR and P_ERR all in Status Register 1 
+        this->wip = (status1 >> WIP) & 0x1; //RDYBSY (device ready/busy status flag)
+        this->wel = (status1 >> WEL) & 0x1;
+        this->e_err = (status1 >> E_ERR_S25HST) & 0x1;
+        this->p_err = (status1 >> P_ERR_S25HST) & 0x1; 
       }
       if (this->wip == 0 && (this->mode == "Erase" || this->mode == "Write")){
         this->mode = "Standby";
@@ -384,14 +460,17 @@ class NORFlash {
 
     void enable_four_byte_addr(){
       //Enable 4-byte addressing mode
-      digitalWrite(cs, LOW);
+      digitalWrite(cs1, LOW);
       SPI.transfer(EN4B);
-      digitalWrite(cs, HIGH);
+      digitalWrite(cs1, HIGH);
     }
 
-    NORFlash(pin_size_t sck, pin_size_t sdi, pin_size_t sdo, pin_size_t reset, pin_size_t cs, pin_size_t cs2, pin_size_t cs3){ //pin_size_t wp){
+    NORFlash(pin_size_t sck, pin_size_t sdi, pin_size_t sdo, pin_size_t reset, pin_size_t cs1, pin_size_t cs2, pin_size_t cs3){
+      //write protect is already disabled with the HW
       this->reset = reset;     
-      this->cs = cs; 
+      this->cs1 = cs1; 
+      this->cs2 = cs2;
+      this->cs3 = cs3;
       this->wp = wp;
       //Begin the SPI Interface for comms with the NOR Flash
       SPI.begin();
@@ -399,19 +478,16 @@ class NORFlash {
       //Change the SPI Settings to allow Clock Frequency of 10MHz (absolute max per datasheet is 133MHz)
       //Most Significant Bit First
       //Data Mode 0 (Clock Polarity = 0 and Clock Phase Angle = 0)
-      SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE0));
-      SPI.setCS(cs);  
+      SPI.beginTransaction(SPISettings(5000000, MSBFIRST, SPI_MODE0)); // see if can actually or need to decrease freq 
+      SPI.setCS(cs1);  
       SPI.setSCK(sck);
       SPI.setRX(sdi);
       SPI.setTX(sdo);
-      pinMode(cs, OUTPUT);
-      //Disable write protect
-      //pinMode(wp, OUTPUT);
-      //digitalWrite(wp, HIGH);
+      pinMode(cs1, OUTPUT);
       pinMode(cs2, OUTPUT);
-      pinMode(cs2, HIGH);
+      digitalWrite(cs2, HIGH);
       pinMode(cs3, OUTPUT);
-      pinMode(cs3, HIGH);
+      digitalWrite(cs3, HIGH);
       //Hold RESET low (Pulse high for reset)
       pinMode(reset, OUTPUT);
       digitalWrite(reset, HIGH);  
